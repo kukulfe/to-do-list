dc6cbeef81f26884d04a216ecb6412c515a70c2e
Veronika Portnyh <vportnyh584@gmail.com>
2025-02-09 15:42:04 +0400

    feat: add creator

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+const buttonParams = {
+  tagName: "button",
+  style: "button",
+  text: "red",
+  attributes: {
+    id: "button",
+    "data-test": "abrakadabra",
+  },
+};
 
-setupCounter(document.querySelector("#counter"));
+const creator = (params) => {
+  const element = document.createElement(params.tagName);
+  element.classList.add(params.style);
+  element.textContent = params.text;
+  for (const key in params.attributes) {
+    element.setAttribute(key, params.attributes[key]);
+  }
+  return element;
+};
+
+const button = creator(buttonParams);
+document.body.append(button);
