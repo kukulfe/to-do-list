9b705724202a517912b6bc8352540beb2a508664
Veronika Portnyh <vportnyh584@gmail.com>
2025-02-28 16:32:53 +0400

    feat: add style for night mode btn

--- src/header/header-params.js
+++ src/header/header-params.js
@@ -1,12 +1,44 @@
 const headerParams = {
   tagName: "header",
-  style: ["max-w-5xl", "border-b-2", "border-cyan-600", "py-2.5", "px-3"],
+  style: [
+    "max-w-5xl",
+    "border-b-2",
+    "border-cyan-600",
+    "py-2.5",
+    "px-3",
+    "m-auto",
+    "justify-between",
+    "flex",
+    "items-center",
+  ],
 };
 
 const titleParams = {
   tagName: "h1",
   text: "To-Do",
-  style: [],
+  style: ["text-cyan-600", "font-medium", "text-3xl"],
 };
 
-export { headerParams, titleParams };
+const buttonParams = {
+  tagName: "button",
+  style: ["w-[40px]", "h-[40px]", "rounded-full", "bg-cyan-600", "relative"],
+};
+
+const wrapperIcon = {
+  tagName: "span",
+  style: [
+    "block",
+    "w-[30px]",
+    "h-[30px]",
+    "absolute",
+    "top-[50%]",
+    "left-[50%]",
+    "translate-x-[-50%]",
+    "translate-y-[-50%]",
+    "bg-center",
+    "bg-no-repeat",
+    "bg-[url(/sun-icon.svg)]",
+  ],
+};
+
+export { headerParams, titleParams, buttonParams, wrapperIcon };
--- src/header/header.js
+++ src/header/header.js
@@ -1,10 +1,19 @@
 import creator from "../core/creator.js";
-import { headerParams, titleParams } from "./header-params.js";
+import {
+  buttonParams,
+  headerParams,
+  titleParams,
+  wrapperIcon,
+} from "./header-params.js";
 
 const headerCreator = () => {
   const headerElement = creator(headerParams);
-
   const titleElement = creator(titleParams);
+  const buttonElement = creator(buttonParams);
+  const wrapperIconElement = creator(wrapperIcon);
+
+  buttonElement.insertAdjacentElement("beforeend", wrapperIconElement);
+  headerElement.insertAdjacentElement("beforeend", buttonElement);
   headerElement.insertAdjacentElement("afterbegin", titleElement);
 
   return headerElement;
