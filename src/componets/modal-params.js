841caeef19d5e056168bb52fb782aca15b9d9d71
Veronika Portnyh <vportnyh584@gmail.com>
2025-04-08 17:38:51 +0400

    feat: add some styles

--- src/componets/modal-params.js
+++ src/componets/modal-params.js
@@ -28,6 +28,63 @@ const fadeBlockParams = {
     "backdrop-blur-[2px]",
   ],
 };
-// ссоздать параметры для fadeBlock
 
-export { formParams, fadeBlockParams };
+const wrapperHeaderParams = {
+  tagName: "div",
+  style: [
+    "flex",
+    "justify-between",
+    "items-center",
+    "border-b-2",
+    "border-cyan-600",
+    "max-w-[340px]",
+    "mb-4",
+    "py-1",
+  ],
+};
+
+const inputParams = {
+  tagName: "input",
+  style: ["max-w-[300px]", "w-full", "outline-none"],
+  attributes: {
+    type: "text",
+    placeholder: "Title",
+  },
+};
+
+const labelParams = {
+  tagName: "label",
+  style: [],
+};
+
+const checkboxParams = {
+  tagName: "input",
+  style: ["real-checkbox", "w-0", "h-0", "absolute", "z-[-1]"],
+  attributes: {
+    type: "checkbox",
+  },
+};
+
+const fakeCheckboxParams = {
+  tagName: "span",
+  style: ["block", "w-5", "h-5", "fake-checkbox", "cursor-pointer"],
+};
+
+const textareaParams = {
+  tagName: "textarea",
+  style: ["outline-none", "w-full", "max-h-[350px]", "min-h-[200px]"],
+  attributes: {
+    placeholder: "Text",
+  },
+};
+
+export {
+  formParams,
+  fadeBlockParams,
+  wrapperHeaderParams,
+  inputParams,
+  labelParams,
+  checkboxParams,
+  fakeCheckboxParams,
+  textareaParams,
+};
--- src/componets/modal.js
+++ src/componets/modal.js
@@ -1,11 +1,34 @@
 import creator from "../core/creator.js";
-import { fadeBlockParams, formParams } from "./modal-params.js";
+import {
+  checkboxParams,
+  fadeBlockParams,
+  fakeCheckboxParams,
+  formParams,
+  inputParams,
+  labelParams,
+  textareaParams,
+  wrapperHeaderParams,
+} from "./modal-params.js";
 
 const createModal = () => {
-  // создать фейд блок и вставить на страницу
   const appContainer = document.querySelector("#app");
-  const formModal = creator(formParams);
   const fadeBlock = creator(fadeBlockParams);
+
+  const formModal = creator(formParams);
+  const wrapperHeader = creator(wrapperHeaderParams);
+  formModal.append(wrapperHeader);
+
+  const input = creator(inputParams);
+  wrapperHeader.append(input);
+
+  const label = creator(labelParams);
+  const checkbox = creator(checkboxParams);
+  const fakeCheckbox = creator(fakeCheckboxParams);
+  label.append(checkbox, fakeCheckbox);
+  wrapperHeader.append(label);
+
+  const texarea = creator(textareaParams);
+  formModal.append(texarea);
   appContainer.append(fadeBlock);
   appContainer.append(formModal);
 };
